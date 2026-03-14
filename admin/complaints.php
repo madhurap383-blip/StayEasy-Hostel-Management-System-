72280947cd2fbbaa0655a46129915c92d4237b31
madhurap383-blip <madhurap383@gmail.com>
2026-03-14 22:26:28 +0530

    Create complaints.php for complaint management

---
+++ admin/complaints.php
@@ -0,0 +1,22 @@
+<?php
+
+// complaints.php
+
+// This file manages the complaint functionalities for the StayEasy Hostel Management System.
+
+// Function to log a complaint
+function logComplaint($userId, $complaintDetails) {
+    // Code to log complaint
+}
+
+// Function to retrieve complaints
+function getComplaints() {
+    // Code to retrieve complaints
+}
+
+// Function to resolve a complaint
+function resolveComplaint($complaintId) {
+    // Code to resolve a complaint
+}
+
+?>
